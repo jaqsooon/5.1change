b47fb4ccad625ab2cb8c09e7cea0007397b19f62
jaqsooon <77469902+jaqsooon@users.noreply.github.com>
2022-10-12 15:56:31 -1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dataStore = require('nedb')
+const app = express();
+
+// NEW: generated port from hosting service 
+const port = process.env.PORT || 3001
+app.listen(port, () => console.log(`starting server at ${port}`))
+
+app.use(express.static('public'));
+app.use(express.json({limit: '1mb'}));
+const database = new dataStore('database.db');
+database.loadDatabase();
+
+app.post('/api', (request, response) => {
+    console.log('I got a request')
+    const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+    database.insert(data);
+    response.json(data);
+});
+
+app.get('/api', (request, response) => {
+    database.find({}, (err, data) => {
+        if(err) {
+            response.end();
+            return;
+        }
+        response.json(data);
+    });
+});
+
+app.delete('/api', (request, response) => {
+    console.log(request.body);
+    const data = request.body;
+    const id = data.id;
+    console.log(id);
+    database.remove({ _id: id }, {}, function(err, remove) {
+        console.log("success");
+    });
+})
